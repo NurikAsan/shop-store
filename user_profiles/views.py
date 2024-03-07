5e4e01cbbd98e7dc248e96ad5ff7a75c25cad4e8
Nurikov <asankylovnursultan@gmail.com>
2024-03-07 19:57:12 +0600

    Created a crud operation for product endpoints is ready

--- user_profiles/views.py
+++ user_profiles/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .serializers import ProductSerializer
+from .models import Product
+from rest_framework import generics
 
-# Create your views here.
+
+class ProductCreateView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDetailsView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+
+class ProductDeleteView(generics.DestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+
+class ProductListView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
